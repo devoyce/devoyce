102326df5015f0794ec78c4548a074b20f5ed9ea
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2025-09-29 12:29:52 -0300

    add shadcn

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "../../config/eslint-config/next";
+import { nextJsConfig } from "@devoyce/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,4 @@
-import { config } from "../../config/eslint-config/react-internal";
+import { config } from "@devoyce/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
